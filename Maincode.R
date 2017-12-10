d9b40f0c873cdac0607df995ba456a21f082d1a0
atapoor <33254133+atapoor@users.noreply.github.com>
2017-12-11 01:15:24 +0200

    My main code
    Initial quick checking of dataset.

---
+++ Maincode.R
@@ -0,0 +1,13 @@
+
+## Shahla Atapoor
+
+# First I read the data
+dataset <- read_csv("E:/1. My Courses/1st Semester/4. Dta Minning/My Project/dataset.csv")
+
+# Quick check the qulity of the column wrist
+hist(dataset$wrist)
+
+# Quick check the qulity of the column activity
+hist(dataset$activity)
+
+# Further analysis are under writing and they will be uploaded soon. 
